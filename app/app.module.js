a89d98364aa1814f9e72cf7cfdfa32409a5995c5
moraneva <moraneva@msu.edu>
2017-03-13 16:19:05 -0400

    playing around with bootstrap v4

--- app/app.module.js
+++ app/app.module.js
@@ -11,12 +11,13 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
 var app_component_1 = require('./app.component');
+var ng_bootstrap_1 = require('@ng-bootstrap/ng-bootstrap');
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule],
+            imports: [platform_browser_1.BrowserModule, ng_bootstrap_1.NgbModule.forRoot()],
             declarations: [app_component_1.AppComponent],
             bootstrap: [app_component_1.AppComponent]
         }), 
